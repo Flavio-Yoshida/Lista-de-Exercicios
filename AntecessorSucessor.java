6aef642e14fb8dd284439d6e279f257829549caa
Flavio-Yoshida <japa_tak@hotmail.com>
2023-05-23 16:47:57 -0300

    commit

--- AntecessorSucessor.java
+++ AntecessorSucessor.java
@@ -4,11 +4,16 @@ public class AntecessorSucessor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite um número inteiro: ");
+        System.out.println("Digite um número inteiro: ");
+        while (!scanner.hasNextInt()) {
+            System.out.println("Entrada inválida. Digite apenas números:");
+            scanner.next(); // Descarta a entrada inválida
+        }
         int numero = scanner.nextInt();
 
         int antecessor = numero - 1;
         int sucessor = numero + 1;
+
 
         System.out.println("O antecessor de " + numero + " é: " + antecessor);
         System.out.println("O sucessor de " + numero + " é: " + sucessor);
